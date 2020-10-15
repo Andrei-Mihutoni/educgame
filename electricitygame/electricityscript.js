5d462b11681b57bfaeb93fc2296a76d99b481b8f
Sylvain Buisson <sylv1612@stud.kea.dk>
2020-10-15 17:02:26 +0200

    added new files...

---
+++ electricitygame/electricityscript.js
@@ -0,0 +1,262 @@
+window.addEventListener("DOMContentLoaded", initGame);
+
+let MOBILE = false;
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  MOBILE = true;
+}
+if (window.innerWidth < 800) {
+  MOBILE = true;
+}
+
+const GAMETIME = 3600; // in ms
+const TIMER_UPDATE_TICK_RATE = 100; // in ms
+
+const NR_OF_PLATES = 10;
+const INITIAL_PLATE_X = 950; // initial plate pos doesn't start at (0,0)
+const INITIAL_PLATE_Y = 530;
+
+//for changing dishwasher image
+const DISHWASHER_EMPTY_PLATES = 5;
+const DISHWASHER_FULL_PLATES = 8;
+
+let svgPoint; //for translating page mouse coords in svg coords
+let points = 0;
+
+let plates = [];
+const Plate = {
+  node: "",
+  collisionNode: "",
+  grabbed: false,
+};
+
+const dishwasher = {
+  nodeFull: "",
+  nodeEmpty: "",
+  nodeClosed: "",
+  collisionNode: "",
+  plates: 0,
+  closed: false,
+};
+
+const sink = {
+  collisionNode: "",
+  plates: 0,
+};
+
+const timer = {
+  timeout: GAMETIME,
+  node: document.querySelector("#timerContent"),
+  running: false,
+};
+
+async function initGame() {
+  let response = await fetch("./kitchen-01.svg");
+  let mySvgData = await response.text();
+  document.querySelector("#svgWrapper").innerHTML = mySvgData;
+
+  svgPoint = document.querySelector("#svgWrapper svg").createSVGPoint();
+  document.querySelector("#startBtn").addEventListener("click", startGame);
+  document.querySelector("#start_again").addEventListener("click", startGame);
+
+  initDishwasher();
+  addPlates();
+  gameLoop();
+}
+
+function startGame() {
+
+  console.log("game starting");
+  timer.timeout = GAMETIME;
+
+
+  document.querySelector("#endScreen").classList.add("hidden");
+  document.querySelector("#startBtn").classList.add("hidden");
+  document.querySelector("#darkenScreen").classList.add("hidden");
+
+  timer.running = true;
+
+}
+
+function initDishwasher() {
+  dishwasher.nodeEmpty = document.querySelector("#dishwasherEmpty");
+  dishwasher.nodeFull = document.querySelector("#dishwasherFull");
+  dishwasher.nodeClosed = document.querySelector("#dishwasherClosed");
+  dishwasher.collisionNode = document.querySelector("#dishwasherCollision");
+
+  dishwasher.nodeFull.classList.add("hidden");
+  dishwasher.nodeEmpty.classList.add("hidden");
+  dishwasher.nodeClosed.classList.add("hidden");
+
+  sink.collisionNode = document.querySelector("#sinkCollision");
+}
+
+function addPlates() {
+  for (let i = 0; i < NR_OF_PLATES; i++) {
+    const newPlate = document.querySelector("#plate").cloneNode(true);
+    newPlate.id = `plate${i}`;
+    document.querySelector("#plates").appendChild(newPlate);
+
+    plate = Object.create(Plate);
+    plate.node = document.querySelector(`#plate${i}`);
+    plate.node.setAttribute("transform", `translate(0,${-i * 15})`);
+    plate.node.classList.add("hovered");
+
+    plate.collisionNode = document.querySelector(`#plate${i} rect`);
+    if (!MOBILE) plate.collisionNode.addEventListener("mousedown", grabPlate);
+    else {
+      //FIXME
+      plate.collisionNode.addEventListener("touchstart", grabPlate);
+      plate.collisionNode.addEventListener("mousedown", grabPlate);
+    }
+
+    plate.grabbed = false;
+
+    plates.push(plate);
+  }
+  //remove initial plate
+  document.querySelector("#plate").parentElement.removeChild(document.querySelector("#plate"));
+}
+
+//translate mouse coords to match svg coords
+function cursorPoint(evt) {
+  svgPoint.x = evt.clientX;
+  svgPoint.y = evt.clientY;
+  return svgPoint.matrixTransform(document.querySelector("#svgWrapper svg").getScreenCTM().inverse());
+}
+
+function grabPlate(event) {
+  event.preventDefault();
+  if (!MOBILE) {
+    event.target.grabbed = !event.target.grabbed;
+    if (event.target.grabbed) {
+      event.target.addEventListener("mousemove", movePlate);
+      event.target.removeEventListener("mousedown", grabPlate);
+      event.target.addEventListener("mouseup", ungrabPlate);
+    } else event.target.removeEventListener("mousemove", movePlate);
+    console.log(event.target.grabbed);
+  } else {
+    //FIXME
+    console.log("Grab");
+    event.target.grabbed = true;
+    if (event.target.grabbed) {
+      event.target.addEventListener("mousemove", movePlate);
+      event.target.removeEventListener("mousedown", grabPlate);
+    } else event.target.removeEventListener("mousemove", movePlate);
+    console.log(event.target.grabbed);
+  }
+}
+
+function ungrabPlate(event) {
+  console.log("Ungrab");
+  event.target.grabbed = false;
+  event.target.removeEventListener("mousemove", movePlate);
+  event.target.addEventListener("mousedown", grabPlate);
+}
+
+function movePlate(event) {
+  event.preventDefault();
+  if (!MOBILE) {
+    event.target.parentElement.setAttribute(
+      "transform",
+      `translate(${parseInt(cursorPoint(event).x) - INITIAL_PLATE_X},${parseInt(cursorPoint(event).y) - INITIAL_PLATE_Y
+      })`
+    );
+  } else {
+    event.target.parentElement.setAttribute(
+      "transform",
+      `translate(${parseInt(cursorPoint(event).x) - INITIAL_PLATE_X},${parseInt(cursorPoint(event).y) - INITIAL_PLATE_Y
+      })`
+    );
+  }
+
+  if (!dishwasher.closed) checkCollision(event.target, dishwasher.collisionNode);
+  else checkCollision(event.target, sink.collisionNode);
+}
+
+function checkCollision(rect1, rect2) {
+  let plateWidth = parseInt(rect1.getAttribute("width")) / 2;
+  let plateHeight = parseInt(rect1.getAttribute("height"));
+  let plateCollisionX = rect1.parentElement.computedStyleMap().get("transform")[0].x.value + INITIAL_PLATE_X;
+  let plateCollisionY =
+    rect1.parentElement.computedStyleMap().get("transform")[0].y.value + INITIAL_PLATE_Y - plateHeight / 2;
+
+  let rect2CollisionX = rect2.x.baseVal.value;
+  let rect2CollisionY = rect2.y.baseVal.value;
+  let rect2Width = parseInt(rect2.getAttribute("width"));
+  let rect2Height = parseInt(rect2.getAttribute("height"));
+
+  if (
+    plateCollisionX < rect2CollisionX + rect2Width &&
+    plateCollisionX + plateWidth > rect2CollisionX &&
+    plateCollisionY < rect2CollisionY + rect2Height &&
+    plateCollisionY + plateHeight > rect2CollisionY
+  ) {
+    if (!dishwasher.closed) addPlateToDishwasher(rect1.parentElement);
+    else addPlateToSink(rect1.parentElement);
+  }
+}
+
+function addPlateToDishwasher(plate) {
+  dishwasher.plates++;
+  plate.parentElement.removeChild(plate);
+  points++;
+}
+
+function addPlateToSink(plate) {
+  sink.plates++;
+  plate.parentElement.removeChild(plate);
+  points++;
+}
+
+function gameLoop() {
+  //PROCESS
+  if (timer.timeout > 0 && timer.running) {
+    timer.timeout--;
+  }
+
+  if (timer.timeout == 0 || points == 10) {
+    endGame();
+    timer.running = false;
+  }
+
+  //VIEW
+  if (timer.timeout > 0 && timer.running) {
+    if (timer.timeout % TIMER_UPDATE_TICK_RATE == 0) {
+      updateTimer();
+    }
+  }
+
+  if (timer.timeout % 10 == 0) updateDishwasherView();
+
+  //LOOP
+  requestAnimationFrame(gameLoop);
+}
+
+function updateDishwasherView() {
+  if (dishwasher.plates < DISHWASHER_EMPTY_PLATES) {
+    dishwasher.nodeEmpty.classList.remove("hidden");
+    dishwasher.nodeClosed.classList.add("hidden");
+    dishwasher.nodeFull.classList.add("hidden");
+  } else if (dishwasher.plates >= DISHWASHER_EMPTY_PLATES && dishwasher.plates < DISHWASHER_FULL_PLATES) {
+    dishwasher.nodeFull.classList.remove("hidden");
+    dishwasher.nodeClosed.classList.add("hidden");
+    dishwasher.nodeEmpty.classList.add("hidden");
+  } else if (dishwasher.plates >= DISHWASHER_FULL_PLATES) {
+    dishwasher.nodeFull.classList.add("hidden");
+    dishwasher.nodeEmpty.classList.add("hidden");
+    dishwasher.nodeClosed.classList.remove("hidden");
+    dishwasher.closed = true;
+  }
+}
+
+function updateTimer() {
+  timer.node.style.width = timer.timeout / (GAMETIME / 100) + "%";
+}
+
+function endGame() {
+  document.querySelector("#darkenScreen").classList.remove("hidden");
+  document.querySelector("#endScreen").classList.remove("hidden");
+  document.querySelector("#endScreen p span").textContent = points;
+
+  timer.running = false;
+}
---
+++ watergame/water.js
@@ -0,0 +1,216 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", init)
+
+let score = 0;
+
+const GAMETIME = 2400; //seting the game time
+let gameReset = false;
+const timer = {
+    timeout: GAMETIME,
+    node: document.querySelector("#timerContent"),
+    running: false,
+};
+
+let instructionsAudio = new Audio("./sounds/instructions.m4a");
+let cheersAudio = new Audio("./sounds/goGoGo.m4a");
+
+
+function init() {
+    fetchSVG();
+    document.querySelector("#startButton").addEventListener("click", start);
+    document.querySelector("#start_again").addEventListener("click", start);
+};
+
+
+function start() {
+    score = 0;
+    timer.running = true;
+    timer.timeout = GAMETIME;
+
+    document.querySelector("#startButton").classList.add("hidden");
+    document.querySelector("#home_button").classList.add("hidden");
+    document.querySelector("#darkenScreen").classList.add("hidden");
+    document.querySelector("#endScreen").classList.add("hidden");
+
+    instructionsAudio.play();
+
+    document.querySelector("#neutralFlower").classList.remove("show");
+    document.querySelector("#happyFlower").classList.remove("show");
+    document.querySelector("#sadFlower").classList.add("show");
+
+    moveDrops();
+    if (!gameReset)
+        gameLoop();
+}
+
+
+
+let mobile = false;    // 
+if (window.innerWidth < 800) {   //setting the mobile "detection" if the screen is smaller than 800px
+    mobile = true;
+};
+
+async function fetchSVG() {  // fetching the SVG
+    // let response = await fetch("./graphics/all-topLeftCorner.svg");
+    let response = await fetch("./graphics/close-up_less_colors2.svg");
+    let mySvgData = await response.text();
+    document.querySelector("#svgHolder").innerHTML = mySvgData;
+    document.querySelector("#sadFlower").classList.remove("hidden"); // showing the initial sad flower
+    document.querySelector("#waterDrop1").setAttribute("transform", "translate(1100,450)"); // placeing the drop in the initial position
+    placeingHands();
+};
+
+
+
+function placeingHands() {  // moving the hands
+    const hands = document.querySelector("#hands");
+    // placeing the hands in the initial position
+    hands.setAttribute("transform", "translate(950,900)");
+};
+
+
+function moveDrops() {  // moving the drop
+    const drop1 = document.querySelector("#waterDrop1");
+
+
+    //adding the animation to the drop
+    drop1.classList.add(`bounce${Math.floor(Math.random() * 1) + 1}`);
+    // drop1.classList.add("bounce2");
+    // console.log(drop1.computedStyleMap().get("transform"));
+    const transformValue = getComputedStyle(drop1).getPropertyValue("transform");
+    console.log(transformValue);
+};
+
+
+
+function gameLoop() {
+    if (timer.timeout > 0 && timer.running) {
+        timer.timeout--;
+    };
+    if (timer.timeout == 0 && timer.running == true) {
+        timer.running = false;
+        endGame();
+    };
+    if (score > 25) {
+        endGame();
+    };
+    if (score == 10) {
+        cheersAudio.play(); //play cheers sound
+    }
+
+    const drop1 = document.querySelector("#waterDrop1");
+    let dropX = drop1.computedStyleMap().get("transform")[0].x.value;  //getting the drop position on x axis
+    let dropY = drop1.computedStyleMap().get("transform")[0].y.value;  //getting the drop position on y axis
+    // console.log(`dropX: ${dropX}`);
+    // let dropY = parseInt(drop1.getClientRects()[0].y);
+
+    // *** process input ***
+    function mouseDown() {
+        if (mobile == true) {
+            document.querySelector("body").addEventListener("mousemove", moveHands); //on screen smaller than 800px it moves the hands on tap
+            // console.log("Mobile screen size");
+        } else {
+            document.querySelector("#hands").addEventListener("mousemove", moveHands);  //on large screen it moves the hands only when hovering on them
+            // console.log("Desktop screen size");
+        }
+    };
+
+    function moveHands(event) {
+        let svg = document.querySelector("#svgHolder svg");  // get the SVG element
+
+        //  ***** Mouse event handler
+        let point = svg.createSVGPoint();  // Create an SVGPoint
+
+        function cursorPoint(evt) {  // Get mouse cursor point in global SVG space
+            point.x = evt.clientX;
+            point.y = evt.clientY;
+            // console.log(point.x);
+            return point.matrixTransform(svg.getScreenCTM().inverse());
+        };
+        document.querySelector("#hands").style.transform = `translate(${cursorPoint(event).x - 150}px, 900px)`;   // moving the hands to the cursor point
+    };
+
+
+    // collision detection
+    function colisionDetection() {
+        const bowlRect = document.querySelector("#bowlRect");
+        const bowlRectX = hands.computedStyleMap().get("transform")[0].x.value; // gets the hand/bowl position on x axis
+        const bowlRectY = hands.computedStyleMap().get("transform")[0].y.value;  // gets the hand/bowl position on y axis
+        const bowlWidth = parseInt(bowlRect.getAttribute("width"));
+        // const bowlHeight = parseInt(bowlRect.getAttribute("height"));
+
+        if (dropX < bowlRectX + bowlWidth &&
+            dropX > bowlRectX &&
+            dropY > bowlRectY) {
+            // collision detected!
+            drop1.setAttribute("class", "");
+            drop1.classList.add(`bounce${Math.floor(Math.random() * 2 + 1)}`);
+            console.log("Collision!")
+            addScore();
+            showFlower();
+        };
+        //     console.log("dropX", dropX, "dropY", dropY, "bowlX", bowlRectX, "bowlY", bowlRectY)
+        //     console.log("bowlRectW", bowlWidth, "bowlRectH", bowlHeight, "bowlRectW+X", bowlWidth + bowlRectX)
+
+
+        function addScore() {                    // adds score on collision of the drops with the bown
+            console.log("Adding to score!");
+            score++;
+            console.log(score);
+        };
+
+        function showFlower() {
+            if (score == 1) {
+                document.querySelector("#bowlInside").classList.add("st1554"); //color the inside of the bowl(make it look waterish)
+
+            } else if (score >= 5 && score < 25) {
+                document.querySelector("#neutralFlower").classList.add("show");
+                document.querySelector("#sadFlower").classList.remove("show");
+                document.querySelector("#sadFlower").classList.add("hidden");
+                // document.querySelector("#bowlInside").setAttribute("class", "");
+                // document.querySelector("#bowlInside").classList.add("st1555"); //color the inside of the bowl(make it look waterish)
+
+            } else if (score > 25) {
+                document.querySelector("#neutralFlower").classList.remove("show");
+                document.querySelector("#happyFlower").classList.add("show");
+
+                //document.querySelector("#bowlInside").setAttribute("class", "");
+                //document.querySelector("#bowlInside").classList.add("st1556"); //color the inside of the bowl(make it look waterish)
+
+            }
+        }
+    };
+
+    // timer: update the visuals on timer count
+    if (timer.timeout > 0 && timer.running) {
+        if (timer.timeout % 100 == 0) {
+            updateTimer();
+        }
+    };
+
+    function updateTimer() {
+        timer.node.style.width = timer.timeout / (GAMETIME / 100) + "%";
+    };
+
+
+
+    mouseDown();
+    colisionDetection();
+    requestAnimationFrame(gameLoop);
+};
+
+
+function endGame() {
+
+    document.querySelector("#endScreen > p > #points").textContent = score;
+    document.querySelector("#endScreen").classList.remove("hidden");
+    document.querySelector("#waterDrop1").setAttribute("class", "");
+    document.querySelector("#startButton").classList.remove("hidden");
+    document.querySelector("#home_button").classList.remove("hidden");
+    document.querySelector("#darkenScreen").classList.remove("hidden");
+
+    timer.running = false;  //stoping the timer
+    gameReset = true;  //reset game
+};
+
+
